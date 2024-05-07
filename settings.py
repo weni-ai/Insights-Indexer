c1141bda28ee2f1549823828a5255a210d5713ed
helllllllder <heldergsds@gmail.com>
2024-05-06 21:20:35 -0300

    feat: update settings

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os
 
 PG_URL = os.environ.get("PG_URL", "")
+PG_FLOWRUN_TABLE_NAME = os.environ.get("PG_FLOWRUN_TABLE_NAME", "flows_flowrun")
 ES_URL = os.environ.get("ES_URL", "")
+ES_FLOWRUN_INDEX_NAME = os.environ.get("ES_FLOWRUN_INDEX_NAME", "flowruns")
 CONNECTION_TYPE = os.environ.get("CONNECTION_TYPE", "single")
