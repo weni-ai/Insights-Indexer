795306a3e277d1a8315efa0bf1458c2575397eee
helllllllder <heldergsds@gmail.com>
2024-05-13 17:22:08 -0300

    feat: fix settings

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ CONNECTION_TYPE = os.environ.get("CONNECTION_TYPE", "pool")
 REDIS_WAIT_TIME_RETRY = os.environ.get("REDIS_WAIT_TIME_RETRY", 10)
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 CONSUMER_THREADS = os.environ.get("CONSUMER_THREADS", 3)
-CONSUMER_MAIN_DELAY = os.environ.get("CONSUMER_THREADS", 10_000)
+CONSUMER_MAIN_DELAY = os.environ.get("CONSUMER_MAIN_DELAY", 1000)
 
 FLOWRUN_PROC_LIST = os.environ.get("FLOWRUN_PROC_LIST", "flowruns:proc")
 FLOWRUN_WAIT_LIST = os.environ.get("FLOWRUN_WAIT_LIST", "flowruns:wait")
