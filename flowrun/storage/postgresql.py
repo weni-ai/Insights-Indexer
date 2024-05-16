3e7853415b5d24d7be9673dbf40162347021b76b
helllllllder <heldergsds@gmail.com>
2024-05-16 14:43:36 -0300

    feat: implement another query for flowrun retrieve, to get the project uuid from the project table instead of org

--- flowrun/storage/postgresql.py
+++ flowrun/storage/postgresql.py
@@ -1,12 +1,19 @@
+import settings
 from shared.storage import BaseRetrieveStorage
 from db.postgres.connection import get_cursor
+
+
+if settings.FLOWRUN_USE_ORG:
+    get_flowrun_by_uuid_sql = "SELECT fr.id, fr.uuid, fr.status, fr.created_on, fr.exited_on, fr.responded, fr.results, fr.delete_reason, fr.exit_type, c.uuid AS contact_uuid, c.name AS contact_name, cu.identity AS contact_urn, f.uuid AS flow_uuid, f.name AS flow_name, o.proj_uuid AS project_uuid FROM flows_flowrun fr INNER JOIN contacts_contact c ON fr.contact_id = c.id INNER JOIN contacts_contacturn cu ON cu.id =(SELECT id from contacts_contacturn WHERE contact_id = c.id FETCH FIRST 1 ROWS ONLY) INNER JOIN flows_flow f ON fr.flow_id = f.id INNER JOIN orgs_org o ON fr.org_id = o.id WHERE fr.uuid = (%s);"
+else:
+    get_flowrun_by_uuid_sql = "SELECT fr.id, fr.uuid, fr.status, fr.created_on, fr.exited_on, fr.responded, fr.results, fr.delete_reason, fr.exit_type, c.uuid AS contact_uuid, c.name AS contact_name, cu.identity AS contact_urn, f.uuid AS flow_uuid, f.name AS flow_name, proj.project_uuid AS project_uuid FROM flows_flowrun fr INNER JOIN contacts_contact c ON fr.contact_id = c.id INNER JOIN contacts_contacturn cu ON cu.id =( SELECT id from contacts_contacturn WHERE contact_id = c.id FETCH FIRST 1 ROWS ONLY) INNER JOIN flows_flow f ON fr.flow_id = f.id INNER JOIN internal_project proj ON fr.org_id = proj.org_ptr_id WHERE fr.uuid =(%s);"
 
 
 class FlowRunPostgreSQL(BaseRetrieveStorage):
     def get_by_pk(self, identifier: str) -> dict:
         with get_cursor() as cur:
             flowrun_query = cur.execute(
-                "SELECT fr.id, fr.uuid, fr.status, fr.created_on, fr.exited_on, fr.responded, fr.results, fr.delete_reason, fr.exit_type, c.uuid AS contact_uuid, c.name AS contact_name, cu.identity AS contact_urn, f.uuid AS flow_uuid, f.name AS flow_name, o.proj_uuid AS project_uuid FROM flows_flowrun fr INNER JOIN contacts_contact c ON fr.contact_id = c.id INNER JOIN contacts_contacturn cu ON cu.id =(SELECT id from contacts_contacturn WHERE contact_id = c.id FETCH FIRST 1 ROWS ONLY) INNER JOIN flows_flow f ON fr.flow_id = f.id INNER JOIN orgs_org o ON fr.org_id = o.id WHERE fr.uuid = (%s);",
+                get_flowrun_by_uuid_sql,
                 (identifier,),
             ).fetchone()
             return flowrun_query
--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ import os
 
 PG_URL = os.environ.get("PG_URL", "")
 PG_FLOWRUN_TABLE_NAME = os.environ.get("PG_FLOWRUN_TABLE_NAME", "flows_flowrun")
+FLOWRUN_USE_ORG = bool(int(os.environ.get("FLOWRUN_USE_ORG", "1")))
+
 ES_URL = os.environ.get("ES_URL", "")
 ES_FLOWRUN_INDEX_NAME = os.environ.get("ES_FLOWRUN_INDEX_NAME", "flowruns")
 CONNECTION_TYPE = os.environ.get("CONNECTION_TYPE", "pool")
