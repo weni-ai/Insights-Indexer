9ad2d7d1562d277ddd3da8f612e363fa7d10042b
helllllllder <heldergsds@gmail.com>
2024-05-21 16:33:33 -0300

    feat: add elasticsearch get last indexed method
    feat: add list_by_modified_on method into FlowRunPostgreSQL

--- flowrun/storage/elasticsearch.py
+++ flowrun/storage/elasticsearch.py
@@ -19,6 +19,20 @@ class FlowRunElasticSearch(GenericStorage):
             return None
         return es_flow_run  # FlowRunElasticSearchDTO.from_dict(es_flow_run)
 
+    def get_last_indexed(self):
+        try:
+            es_flow_run = get_connection().search(
+                index=self._index_name,
+                body={"size": 1},
+            )["hits"]["hits"][0]["_source"]
+        except (AttributeError, TypeError, IndexError) as err:
+            print("[-] <Warning> Elasticsearch Get error: ", type(err), err)
+            return None
+        return es_flow_run
+
     def insert(self, new_obj: dict) -> bool:
         es_flow_run = get_connection().index(index=self._index_name, body=new_obj)
         return es_flow_run["result"] == "created"
+
+    def bulk_insert(self, batch) -> bool:
+        pass
--- flowrun/storage/postgresql.py
+++ flowrun/storage/postgresql.py
@@ -8,6 +8,8 @@ if settings.FLOWRUN_USE_ORG:
 else:
     get_flowrun_by_uuid_sql = "SELECT fr.id, fr.uuid, fr.status, fr.created_on, fr.exited_on, fr.responded, fr.results, fr.delete_reason, fr.exit_type, c.uuid AS contact_uuid, c.name AS contact_name, cu.identity AS contact_urn, f.uuid AS flow_uuid, f.name AS flow_name, proj.project_uuid AS project_uuid FROM flows_flowrun fr INNER JOIN contacts_contact c ON fr.contact_id = c.id INNER JOIN contacts_contacturn cu ON cu.id =( SELECT id from contacts_contacturn WHERE contact_id = c.id FETCH FIRST 1 ROWS ONLY) INNER JOIN flows_flow f ON fr.flow_id = f.id INNER JOIN internal_project proj ON fr.org_id = proj.org_ptr_id WHERE fr.uuid =(%s);"
 
+list_flowrun_by_modified_on_sql = "SELECT fr.id, fr.uuid, fr.status, fr.created_on, fr.exited_on, fr.responded, fr.results, fr.delete_reason, fr.exit_type, c.uuid AS contact_uuid, c.name AS contact_name, cu.identity AS contact_urn, f.uuid AS flow_uuid, f.name AS flow_name, o.proj_uuid AS project_uuid FROM flows_flowrun fr INNER JOIN contacts_contact c ON fr.contact_id = c.id INNER JOIN contacts_contacturn cu ON cu.id =( SELECT id from contacts_contacturn WHERE contact_id = c.id FETCH FIRST 1 ROWS ONLY) INNER JOIN flows_flow f ON fr.flow_id = f.id INNER JOIN orgs_org o ON fr.org_id = o.id WHERE fr.status NOT IN('A', 'W') AND fr.modified_on > (%s) LIMIT (%s);"
+
 
 class FlowRunPostgreSQL(BaseRetrieveStorage):
     def get_by_pk(self, identifier: str) -> dict:
@@ -16,4 +18,14 @@ class FlowRunPostgreSQL(BaseRetrieveStorage):
                 get_flowrun_by_uuid_sql,
                 (identifier,),
             ).fetchone()
+            return flowrun_query
+
+    def list_by_modified_on(
+        self, modified_on: str, limit: int = settings.FLOW_RUN_BATCH_LIMIT
+    ) -> list[dict]:
+        with get_cursor() as cur:
+            flowrun_query = cur.execute(
+                list_flowrun_by_modified_on_sql,
+                (modified_on, limit),
+            ).fetchall()
             return flowrun_query
