b665a7e8b70b366d545699c230e6a097c9a1c9f5
Marcus Vinícius <marcusvinici25@gmail.com>
2025-02-04 16:15:04 -0300

    adding loop logic in from_obj_list

--- shared/processors.py
+++ shared/processors.py
@@ -30,23 +30,38 @@ class BulkObjectETLProcessor:
         
         for org in orgs:
             org_id = org if type(org) is int else org.get("id")
-            org_start_time = time.time()  # Tempo de início do processo por organização
+            org_start_time = time.time()  # Process start time by organization
 
-            # Get last indexed timestamp document on the storage_to
+            # Get the last indexed timestamp
             last_indexed_at = self.storage_to.get_last_indexed_timestamp(org_id)
 
-            # [E]xtract the obj list from the From Storage, filtered by the last indexed timestamp
-            extract_start_time = time.time()
-            from_obj_list = self.storage_from.list_by_timestamp_and_org(
-                modified_on=last_indexed_at, org_id=org_id
-            )
-            extract_elapsed_time = time.time() - extract_start_time
-            logger.info(f"Extraction for org {org_id} took {extract_elapsed_time:.4f} seconds")
+            # Retry loop up to 8 times
+            max_retries = 8
+            for attempt in range(max_retries):
+                extract_start_time = time.time()
 
-            if len(from_obj_list) == 0:  # if there's no objects on the list
+                # Extracts objects modified after last_indexed_at
+                from_obj_list = self.storage_from.list_by_timestamp_and_org(
+                    modified_on=last_indexed_at, org_id=org_id
+                )
+                extract_elapsed_time = time.time() - extract_start_time
+                logger.info(
+                    f"Extraction attempt {attempt+1}/{max_retries} for org {org_id} took {extract_elapsed_time:.4f} seconds"
+                )
+
+                # If it found objects, it exits the loop and continues the logic
+                if from_obj_list:
+                    break  
+
+                # If not found, increment last_indexed_at by 1 minute and try again
+                last_indexed_at += timedelta(minutes=1)
+
+            # If after 8 attempts the list is still empty, wait and continue
+            if not from_obj_list:
                 time.sleep(settings.EMPTY_ORG_SLEEP)
                 continue
 
+            # Further processing
             transformed_objects = []
             transform_start_time = datetime.now()
 
